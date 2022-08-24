bf00f1b709e298259065783d2f8a3a8d3ef133ff
pktiwari19 <pktiwari19@gmail.com>
2022-08-24 19:56:15 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,14 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export dockerpath="pktiwari19/mlpredictor:v1.0"
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy microservicesapp --image=$dockerpath
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get deployment,pods
 
 # Step 4:
 # Forward the container port to a host
-
